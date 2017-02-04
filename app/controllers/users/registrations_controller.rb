6336d853f92d58679ad61483e853ff6287a3c4a6
hayashi_kengo <hayashikengojp@gmail.com>
2017-02-04 18:03:26 +0900

    Userの詳細ページを追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
+  # GET /resource/edit
+  # def show
+  #   super
+  # end
+
   # GET /resource/sign_up
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  devise_scope :user do
+    get 'users/:id', to: 'devise/registrations#show', as: 'users'
+  end
   
   root 'home#index'
   resources :home, only: [:index, :show]
