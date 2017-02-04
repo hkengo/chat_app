9e499e53682c3f6815b72262cbc2c60a06e9f85c
hayashi_kengo <hayashikengojp@gmail.com>
2017-02-04 16:31:16 +0900

    ログイン・ログアウト後の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    home_index_path
+  end
+  
+  # ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    home_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  
+  root 'home#index'
   resources :home, only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
