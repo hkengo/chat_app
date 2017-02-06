7a941d0cba38742dae93cfd73d8e1c9a778a26de
hayashi_kengo <hayashikengojp@gmail.com>
2017-02-06 12:58:27 +0900

    RoomsController#showを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
+  root 'home#index'
+  resources :home, only: [:index, :show]
+  resources :rooms, only: [:show]
+
   devise_for :users
   devise_scope :user do
     get 'users/:id', to: 'devise/registrations#show', as: 'users'
   end
-  
-  root 'home#index'
-  resources :home, only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
