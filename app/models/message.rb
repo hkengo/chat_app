314b162f824c1ea0b980018d375c3b87ff28b33b
hayashi_kengo <hayashikengojp@gmail.com>
2017-02-06 14:26:04 +0900

    annotate

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  content    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :integer
+#
+
 class Message < ApplicationRecord
   belongs_to :room
   
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  content    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :integer
+#
+
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
